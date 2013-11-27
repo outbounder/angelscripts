94560a611cad4f308c8f9d4e122b51492feeebf8
Boris Filipov <obiwonn@gmail.com>
2013-11-27 17:52:41 +0200

    improved release script

--- src/release.js
+++ src/release.js
@@ -7,30 +7,26 @@ module.exports = function(angel) {
       options.cwd = process.cwd()
     options = angel.cloneDNA(options)
 
-    exec("git pull --ff #{remote} #{target}", options, function(err, result){
-      if(err) return next(err)
+    fs.readFile(options.cwd+"/package.json", function(err, data){
 
-      fs.readFile(options.cwd+"/package.json", function(err, data){
-
-        var p = JSON.parse(data)
-        var newVersion = p.version.split(".");
-        newVersion[2] = (parseInt(newVersion[2])+1).toString();
-        newVersion = newVersion.join(".");
-        
-        options.newVersion = newVersion
-        p.version = options.newVersion
-
-        fs.writeFile(options.cwd+"/package.json", JSON.stringify(p, null, 2), function(err){
-          if(err) return next(err)
-          exec([
-            "git add package.json",
-            "git commit -am '#{newVersion} release'",
-            "git push #{remote} #{target}"
-          ], options, next)
-        })
-      });
+      var p = JSON.parse(data)
+      var newVersion = p.version.split(".");
+      newVersion[2] = (parseInt(newVersion[2])+1).toString();
+      newVersion = newVersion.join(".");
       
-    })
+      options.newVersion = newVersion
+      p.version = options.newVersion
+
+      fs.writeFile(options.cwd+"/package.json", JSON.stringify(p, null, 2), function(err){
+        if(err) return next(err)
+        exec([
+          "git add package.json",
+          "git commit -am '#{newVersion} release'",
+          "git push #{remote} #{target}"
+        ], options, next)
+      })
+    });
+      
   })
 
   angel.on("release :base to :remote at :target :cwd? :dryrun?", function(options, next){
