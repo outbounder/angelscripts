b060f734c2a061cb72eea956dabd271eda8cc8e4
Boris Filipov <obiwonn@gmail.com>
2013-11-28 18:56:37 +0200

    switch to collectSeries

--- src/git-release.js
+++ src/git-release.js
@@ -1,20 +1,18 @@
-var series = require("reactions").make.series
+var series = require("reactions").make.collectSeries
 var exec = require("shellreactions-exec").exec
 var fs = require('fs')
 
 var incrementPackageJSONVersion = function(options, next){
-  fs.readFile(options.cwd+"/package.json", function(err, data){
+  fs.readFile(process.cwd()+"/package.json", function(err, data){
     if(err) return next(err)
     var p = JSON.parse(data)
     var newVersion = p.version.split(".")
     newVersion[2] = (parseInt(newVersion[2])+1).toString()
     newVersion = newVersion.join(".")
     
-    options.newVersion = newVersion
-    options.oldVersion = p.version
-    p.version = options.newVersion
+    p.version = newVersion
 
-    fs.writeFile(options.cwd+"/package.json", JSON.stringify(p, null, 2), function(err){
+    fs.writeFile(process.cwd()+"/package.json", JSON.stringify(p, null, 2), function(err){
       if(err) return next(err)
       next(null, p)
     })
@@ -30,7 +28,7 @@ module.exports = function(angel) {
 
   angel.on("git-release :target to :remote", function(options, next){
     var run = series([
-      incrementPackageJSONVersion("cmdData"),
+      incrementPackageJSONVersion,
       exec([
         "git add package.json",
         "git commit -m '{newVersion} release'",
